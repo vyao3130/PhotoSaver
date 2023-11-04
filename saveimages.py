85b00ad5da21d6a80abe59a08c43ab46d6d5016e
accelSheep <48867277+vyao3130@users.noreply.github.com>
2023-11-03 23:03:18 -0400

    Updated Functionality
    Updated Functionality and added blog of choice as blog to work from

--- saveimages.py
+++ saveimages.py
@@ -2,23 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+from bs4 import BeautifulSoup
 
-def scroll_down(self):
+
+def scroll_down(driver):
     """A method for scrolling the page."""
 
     # Get scroll height.
-    last_height = self.driver.execute_script("return document.body.scrollHeight")
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
     while True:
 
         # Scroll down to the bottom.
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         # Wait to load the page.
         time.sleep(2)
 
         # Calculate new scroll height and compare with last scroll height.
-        new_height = self.driver.execute_script("return document.body.scrollHeight")
+        new_height = driver.execute_script("return document.body.scrollHeight")
 
         if new_height == last_height:
 
@@ -26,12 +28,60 @@ def scroll_down(self):
 
         last_height = new_height
 
+def save_bigText(bigText):
+    """
+        Parses the big text returned by the image_box 
+        bigText (string) : big mass of html text from /archive tumblr
+    """
+    # getting all this text can take a while so we'll also save it into a temp text file
+    with open("bigText.html", "w",  encoding="utf-8") as f:
+        f.write(bigText)
 
-def main():
+def parse_bigText(file_name):
+    """
+        Parse the big mass of html taken from the tumblr archive into
+        usable chunks.
+        The chunks have the format of list[]
+        return: list of tumblr posts as links list[str]
+    """
+    post_dictionary=[]
+    with open(file_name, "r", encoding="utf-8") as f:
+        soup_archive = BeautifulSoup(f, "html.parser")
+        post_list = soup_archive.find_all(attrs={"class":"oKaff QmZ0e"})
+ 
+        for post in post_list:
+            htmlized_link = BeautifulSoup(str(post), "html.parser")
+            cleaned_posts = htmlized_link.find_all("a","oKaff QmZ0e")
+            split_post = str(cleaned_posts).split(" ")
+            post_dictionary.append(split_post[3][6:-1])
+        print(type(post_dictionary))
+        print(post_dictionary)
+        return post_dictionary
+
+def get_archive_html():
+    """
+        Gets the entire html source code of a dynamic webpage and saves it to an html file.
+        todo: put link somewhere and name html file after link in some way
+    """
     driver = webdriver.Chrome()
     driver.get("https://star-nomads.tumblr.com/archive")
-    text_box = driver.find_element(by=By.CLASS_NAME, value="TlRfv")
-    print(text_box)
+    scroll_down(driver)
+    bigText_archive = driver.page_source
+    save_bigText(bigText_archive)
+
+def main():
+    # get_archive_html()
+    # make driver
+    # image_box = driver.find_element(by=By.CLASS_NAME, value="TlRfv")
+    # image_box = driver.page_source
+    # print(image_box.get_attribute("innerHTML"))
+
+    # bigText_archive = image_box.get_attribute("innerHTML")
+
+    links2posts = parse_bigText("bigText.html")
+    for link in links2posts:
+        process_link(link)
+    # parse text
 
 
 if __name__ == "__main__":
