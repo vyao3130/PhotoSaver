9c05067c3beeb9ffd0082c83dea56dfe07308037
accelSheep <48867277+vyao3130@users.noreply.github.com>
2023-11-05 00:41:44 -0400

    continued updating functionality

--- saveimages.py
+++ saveimages.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import sys
 from bs4 import BeautifulSoup
 
 
@@ -28,13 +29,13 @@ def scroll_down(driver):
 
         last_height = new_height
 
-def save_bigText(bigText):
+def save_bigText(bigText, file_name):
     """
         Parses the big text returned by the image_box 
         bigText (string) : big mass of html text from /archive tumblr
     """
     # getting all this text can take a while so we'll also save it into a temp text file
-    with open("bigText.html", "w",  encoding="utf-8") as f:
+    with open(file_name, "w",  encoding="utf-8") as f:
         f.write(bigText)
 
 def parse_bigText(file_name):
@@ -58,29 +59,57 @@ def parse_bigText(file_name):
         print(post_dictionary)
         return post_dictionary
 
-def get_archive_html():
+def get_archive_html(link):
     """
-        Gets the entire html source code of a dynamic webpage and saves it to an html file.
-        todo: put link somewhere and name html file after link in some way
+    Gets the entire html source code of a dynamic webpage and saves it to an html file.
+    todo: put link somewhere and name html file after link in some way
     """
+    # link = "https://star-nomads.tumblr.com/archive"
     driver = webdriver.Chrome()
-    driver.get("https://star-nomads.tumblr.com/archive")
+    driver.get(link)
     scroll_down(driver)
     bigText_archive = driver.page_source
-    save_bigText(bigText_archive)
+    if "archive" in link:
+        html_link_name = link.split(".")
+        html_link_name2 = html_link_name[0][8:]
+        save_bigText(bigText_archive, file_name=html_link_name2)
+        return html_link_name2
+    elif "post" in link:
+        html_link_name = link.split("post/")
+        html_link_name2 = html_link_name[1]
+        save_bigText(bigText_archive, file_name=html_link_name2)
+        return html_link_name2
+    else:
+        print(f"The link {link} can't be parsed for file use.")
+        sys.exit(1)
 
+def process_link(link):
+    """
+    Processes the link to a tumblr post- saves the image link, source, and original post link into a dictionary.
+    Param: link (str)
+    Return: dict{"img_link":str, "source":str, "post_link":str}
+    """
+    post_html_file = get_archive_html(link)
+    with open(post_html_file, 'r', encoding='utf-8') as f:
+        soup_html = BeautifulSoup(f, "html.parser")
+        img_list = soup_html.find_all(attrs={"class":"post_media_photo_anchor"})
+        for img in img_list:
+            ac_img_link = str(img).split("data-big-photo=")[1]
+            # print(ac_img_link)
+            ac_img_link1 = ac_img_link.split(" ")[0]
+        print(ac_img_link1)
 def main():
     # get_archive_html()
     # make driver
     # image_box = driver.find_element(by=By.CLASS_NAME, value="TlRfv")
     # image_box = driver.page_source
     # print(image_box.get_attribute("innerHTML"))
-
+    process_link("https://star-nomads.tumblr.com/post/732790132063731712")
     # bigText_archive = image_box.get_attribute("innerHTML")
 
-    links2posts = parse_bigText("bigText.html")
-    for link in links2posts:
-        process_link(link)
+    # links2posts = parse_bigText("bigText.html")
+    # for link in links2posts:
+    #     process_link(link)
     # parse text
 
 
